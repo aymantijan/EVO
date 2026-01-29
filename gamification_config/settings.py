696240a90593fdfc99c51e92f00b7eba3136751b
aymantijan <tijanayman99@gmail.com>
2026-01-29 20:13:35 +0100

    Add CSRF_TRUSTED_ORIGINS for Railway

--- gamification_config/settings.py
+++ gamification_config/settings.py
@@ -176,6 +176,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://evpe.up.railway.app',
+    'https://*.railway.app',
 ]
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
